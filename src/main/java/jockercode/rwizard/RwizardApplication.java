8b8c62a9656012907f345a34be8d919b3114d32d
Evander <rojasevander@gmail.com>
2024-01-09 19:27:23 -0500

    init

---
+++ src/main/java/jockercode/rwizard/RwizardApplication.java
@@ -0,0 +1,13 @@
+package jockercode.rwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RwizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jockercode/rwizard/RwizardApplicationTests.java
@@ -0,0 +1,13 @@
+package jockercode.rwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
